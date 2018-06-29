f222d1cdf365372e890924744d44f7d26500c358
Temzasse <teemu.taskula@taitounited.fi>
2018-06-29 10:29:53 +0300

    Add more jsdocs

--- src/reducktion/index.js
+++ src/reducktion/index.js
@@ -1,25 +1,70 @@
+// @ts-check
+
 import { createAction, handleActions } from 'redux-actions';
 import { all } from 'redux-saga/effects';
 
 // Helpers -------------------------------------------------------------------
+
+// JSDoc typedefs
+
+/**
+ * @typedef {Object.<string, Function>} Action
+ */
+
+/**
+ * @typedef {Object.<string, Function>} Selectors
+ */
+
+/**
+ * @typedef {Object.<string, string>} Types
+ */
+
+/**
+ * @typedef {Object} DuckFuncs
+ * @property {Function} actions
+ * @property {Function} selectors
+ * @property {Function} reducer
+ * @property {Function} operations
+ * @property {Function} inject
+ */
+
+ /**
+ * @typedef {Object} Duck
+ * @property {Function} _run
+ * @property {Function} _fillDeps
+ * @property {Function} getOperations
+ * @property {Function} getReducer
+ * @property {String} name
+ * @property {Action} actions
+ * @property {Types} types
+ * @property {Selectors} selectors
+ * @property {Object.<string, any>} initialState
+ */
+
 const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
 
 const isFunction = f => f && {}.toString.call(f) === '[object Function]';
 
-function createTypes(base, actionsArray = []) {
-  const res = {};
-  actionsArray.forEach(type => {
-    res[type] = `${base}/${type}`;
-  });
-  return res;
+/**
+ * Helper function to create prefixed types for a duck.
+ * @param {string} prefix
+ * @param {string[]} actionsArray
+ * @returns {Types}
+ */
+function createTypes(prefix, actionsArray = []) {
+  return actionsArray.reduce((acc, type) => {
+    acc[type] = `${prefix}/${type}`;
+    return acc;
+  }, {});
 }
 // ---------------------------------------------------------------------------
 
 /**
  * Creates a ducks model and returns chainable functions to define futher props.
- * @param {string} modelName 
- * @param {string[]} typeList 
- * @param {Object} initialState 
+ * @param {string} modelName
+ * @param {string[]} typeList
+ * @param {Object.<string, any>} initialState
+ * @returns {DuckFuncs} Chainable duck functions
  */
 export const createModel = (modelName, typeList, initialState) => {
   const types = createTypes(modelName, typeList);
@@ -37,12 +82,14 @@ export const createModel = (modelName, typeList, initialState) => {
   }, {});
 
   /**
-   * Creates action creators and thunks. 
-   * @param {Function} actionsFunc 
+   * Creates action creators and thunks.
+   * @param {Function} actionsFunc
+   * @returns {DuckFuncs}
    */
   funcs.actions = actionsFunc => {
     const actionsObj = actionsFunc({ types }) || {};
 
+    // @ts-ignore
     Object.entries(actionsObj).forEach(([actionName, value]) => {
       if (isFunction(value)) {
         // Handle thunks -> provide necessary data: self and dependencies
@@ -62,7 +109,8 @@ export const createModel = (modelName, typeList, initialState) => {
 
   /**
    * Extends auto-generated selectors for the duck.
-   * @param {Function} selectorsFunc 
+   * @param {Function} selectorsFunc
+   * @returns {DuckFuncs}
    */
   funcs.selectors = selectorsFunc => {
     const selectorsObj = selectorsFunc({ name: modelName }) || {};
@@ -71,19 +119,32 @@ export const createModel = (modelName, typeList, initialState) => {
   };
 
   /**
-   * Defines a curried function that is later provided with types, initial state, and dependencies.
-   * @param {Function} reducerFunc 
+   * Defines a curried function for reducer that is later provided with
+   * types, initial state, and dependencies.
+   * @param {Function} reducerFunc
+   * @returns {DuckFuncs}
    */
   funcs.reducer = reducerFunc => {
     reducer = reducerFunc;
     return funcs;
   };
 
+  /**
+   * Defines a curried function for operations that is later provided with
+   * types, initial state, and dependencies.
+   * @param {Function} operationsFunc
+   * @returns {DuckFuncs}
+   */
   funcs.operations = operationsFunc => {
     operations = operationsFunc;
     return funcs;
   };
 
+  /**
+   * Defines the dependencies of the duck which should be injected to it later.
+   * @param {...string} deps
+   * @returns {DuckFuncs}
+   */
   funcs.inject = (...deps) => {
     deps.forEach(depName => {
       dependencies[depName] = null;
@@ -92,6 +153,10 @@ export const createModel = (modelName, typeList, initialState) => {
     return funcs;
   };
 
+  /**
+   * Fills in the dependencies that were requested when calling inject.
+   * @param {Object.<string, Duck>} deps
+   */
   const _fillDeps = deps => {
     Object.keys(dependencies).forEach(dep => {
       if (deps[dep]) {
@@ -109,6 +174,9 @@ export const createModel = (modelName, typeList, initialState) => {
     });
   };
 
+  /**
+   * Run the curried reducer and operations functions with the necessary data.
+   */
   const _run = () => {
     // Run curried functions with own types and dependencies
     const reducerObj = reducer({ types, initialState, ...dependencies }) || {};
@@ -116,8 +184,16 @@ export const createModel = (modelName, typeList, initialState) => {
     operations = operations({ types, ...dependencies }) || [];
   };
 
+  /**
+   * Get reducer for the duck.
+   * @returns {Function}
+   */
   const getReducer = () => reducer;
 
+  /**
+   * Get operations for the duck.
+   * @returns {Function}
+   */
   const getOperations = () => {
     function* ops() {
       yield all(operations);
@@ -125,6 +201,10 @@ export const createModel = (modelName, typeList, initialState) => {
     return ops;
   };
 
+  /**
+   * Collect and return all the properties of the duck.
+   * @returns {Duck}
+   */
   funcs.create = () => {
     return {
       name: modelName,
@@ -142,12 +222,18 @@ export const createModel = (modelName, typeList, initialState) => {
   return funcs;
 };
 
+/**
+ * Create ducks.
+ * @param {Duck[]} ducks 
+ * @returns {Object.<string, Duck>}
+ */
 export const createDucks = ducks => {
   const ducksByName = ducks.reduce((acc, val) => {
     acc[val.name] = val;
     return acc;
   }, {});
 
+  // @ts-ignore
   Object.values(ducksByName).forEach(duck => {
     duck._fillDeps(ducksByName);
     duck._run();
